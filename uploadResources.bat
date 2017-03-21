54df87958c067613990d8f53b53332270418b52d
ajinkya.raut <ajinkya.raut@enzigma.in>
2017-03-21 14:38:25 +0530

    - Test Jenkins New Script

--- uploadResources.bat
+++ uploadResources.bat
@@ -3,4 +3,7 @@
 cd static_resources
 del ..\src\staticresources\FoundationConnect.resource
 7z a -tzip -r ..\src\staticresources\FoundationConnect.resource *
+cd ..\src\staticresources
+dir
+echo ^<?xml version="1.0" encoding="UTF-8"?^>^<StaticResource xmlns="http://soap.sforce.com/2006/04/metadata"^>^<cacheControl^>Public^</cacheControl^>^<contentType^>application/x-zip-compressed^</contentType^>^</StaticResource^> >
 pause

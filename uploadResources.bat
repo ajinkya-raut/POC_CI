56ecdf3ae60ab72a21be4ed6b7d37d9ec8d50b80
ajinkya.raut <ajinkya.raut@enzigma.in>
2017-03-21 14:56:55 +0530

    -- Test Script

--- uploadResources.bat
+++ uploadResources.bat
@@ -5,5 +5,5 @@ del ..\src\staticresources\FC_CI_New.resource
 7z a -tzip -r ..\src\staticresources\FC_CI_New.resource *
 cd ..\src\staticresources
 dir
-echo ^<?xml version="1.0" encoding="UTF-8"?^>^<StaticResource xmlns="http://soap.sforce.com/2006/04/metadata"^>^<cacheControl^>Public^</cacheControl^>^<contentType^>application/x-zip-compressed^</contentType^>^</StaticResource^> >FC_CI_New.xml
+echo ^<?xml version="1.0" encoding="UTF-8"?^>^<StaticResource xmlns="http://soap.sforce.com/2006/04/metadata"^>^<cacheControl^>Public^</cacheControl^>^<contentType^>application/x-zip-compressed^</contentType^>^</StaticResource^> >FC_CI_New.resource-meta.xml
 pause

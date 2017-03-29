96dde1fc6011f99da7a5c293c0e8746edf735a44
ajinkya.raut <ajinkya.raut@enzigma.in>
2017-03-29 18:14:35 +0530

    -- TEST Script Batch File

--- uploadResources.bat
+++ uploadResources.bat
@@ -1,9 +1,14 @@
 @echo off
 
+set PATH=%PATH%;C:\Program Files\7-Zip\
+echo %PATH%
+
+
+@echo off
+
 cd static_resources
 del ..\src\staticresources\FC_CI_New.resource
 7z a -tzip -r ..\src\staticresources\FC_CI_New.resource *
 cd ..\src\staticresources
 dir
 echo ^<?xml version="1.0" encoding="UTF-8"?^>^<StaticResource xmlns="http://soap.sforce.com/2006/04/metadata"^>^<cacheControl^>Public^</cacheControl^>^<contentType^>application/x-zip-compressed^</contentType^>^</StaticResource^> >FC_CI_New.resource-meta.xml
-pause
